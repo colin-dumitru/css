28c641d9750f6af652b028367b3698dc6ad71f19
Catalin Dumitru <colin.dumitru@gmail.com>
2013-05-27 18:12:20 +0300

    minor fix

--- src/edu/css/java/ReportGenerator.java
+++ src/edu/css/java/ReportGenerator.java
@@ -7,9 +7,7 @@ import com.itextpdf.text.pdf.PdfWriter;
 import edu.css.model.Exam;
 import edu.css.model.Student;
 import edu.css.operations.AdmissionHelper;
-import edu.css.operations.DAOLoader;
 import edu.css.operations.ExamDAO;
-import edu.css.operations.StudentDAO;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -31,7 +29,7 @@ public class ReportGenerator {
     private ExamDAO examDAO;
 
     public ReportGenerator(List<Student> studentList, ExamDAO examDAO, String outputFileName) {
-        assert studentList != null && !studentList.isEmpty() : "studentList should not be null and contain at least one student";
+        assert studentList != null : "studentList should not be null";
         assert examDAO != null : "examDAO is null";
         assert outputFileName != null && outputFileName.length() > 0 : "invalid outputFileName";
         this.studentList = studentList;
@@ -39,37 +37,36 @@ public class ReportGenerator {
         this.outputFileName = getOutputFileName(outputFileName);
     }
 
-    private String getOutputFileName(String outputFileName){
-        if(outputFileName.endsWith(".pdf")){
+    private String getOutputFileName(String outputFileName) {
+        if (outputFileName.endsWith(".pdf")) {
             return outputFileName;
         }
-        return outputFileName+".pdf";
+        return outputFileName + ".pdf";
     }
 
     public void generate() {
         try {
-        Document document = new Document(PageSize.A4,Const,Const,Const,Const);
-        assert outputFileName != null : "Assertion Fail, invalid outputPath";
-        PdfWriter.getInstance(document, new FileOutputStream(outputFileName,false));
-        document.open();
+            Document document = new Document(PageSize.A4, Const, Const, Const, Const);
+            assert outputFileName != null : "Assertion Fail, invalid outputPath";
+            PdfWriter.getInstance(document, new FileOutputStream(outputFileName, false));
+            document.open();
 
-        Paragraph p  = new Paragraph("Admission report");
-        document.add(p);
+            Paragraph p = new Paragraph("Admission report");
+            document.add(p);
 
-        PdfPTable table = createTableHeader();
-        addTableData(table);
+            PdfPTable table = createTableHeader();
+            addTableData(table);
 
-        document.add(table);
-        document.close();
-        }
-        catch (DocumentException e){
+            document.add(table);
+            document.close();
+        } catch (DocumentException e) {
             e.printStackTrace();
-        }catch (FileNotFoundException e){
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
     }
 
-    private PdfPTable createTableHeader(){
+    private PdfPTable createTableHeader() {
         PdfPTable table = new PdfPTable(6);
         table.setSpacingBefore(15);
         table.setSpacingAfter(15);
@@ -98,7 +95,7 @@ public class ReportGenerator {
         return table;
     }
 
-    private void addTableData(PdfPTable table){
+    private void addTableData(PdfPTable table) {
         assert studentList != null : "Assertion Fail, studentList cannot be null";
         for (Student student : studentList) {
             Exam exam = examDAO.getExamForStudent(student);
@@ -106,8 +103,8 @@ public class ReportGenerator {
             assert exam != null : "Assertion Fail, exam for student cannot be null";
             assert student.getAverage() <= 10 : "Assertion Fail, student average cannot be more than 10";
             assert exam.getMark() <= 10 : "Assertion Fail, exam mark cannot be more than 10";
-            assert AdmissionHelper.getPassingMark(student,exam) >= 5.0 && AdmissionHelper.passed(student,exam) ||
-                   AdmissionHelper.getPassingMark(student,exam) < 5.0 && !AdmissionHelper.passed(student,exam) :
+            assert AdmissionHelper.getPassingMark(student, exam) >= 5.0 && AdmissionHelper.passed(student, exam) ||
+                    AdmissionHelper.getPassingMark(student, exam) < 5.0 && !AdmissionHelper.passed(student, exam) :
                     "Assertion Fail, invalid admission rate calculation";
 
             table.addCell(getCellValue(student.getId()));
@@ -121,8 +118,8 @@ public class ReportGenerator {
         assert table.getRows().size() == studentList.size() + 1 : "table nr of rows: " + table.getRows().size() + " <> " + studentList.size() + 1; //+1 for header
     }
 
-    private String getCellValue(Object value){
-        if(value == null){
+    private String getCellValue(Object value) {
+        if (value == null) {
             return "undefined";
         }
         return value.toString();
